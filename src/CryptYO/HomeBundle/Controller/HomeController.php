24407bee08b5f29e86d450bc25756389f3457938
ErwanHaquet <ehaquet.dev@gmail.com>
2015-10-06 14:11:14 +0200

    ajout du bundle CryptYO et de l'index dans home

--- src/CryptYO/HomeBundle/Controller/HomeController.php
+++ src/CryptYO/HomeBundle/Controller/HomeController.php
@@ -3,7 +3,6 @@
 namespace CryptYO\HomeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 
 class HomeController extends Controller
